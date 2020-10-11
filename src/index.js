8e8b1d9c29807f044324ca4720e0b21fdb6f4502
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-10-11 23:23:51 +0300

    Добавляет функционал изменения темы

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './css/styles.css';
 
 const refs = {
   menuContainer: document.querySelector('.js-menu'),
+  toggleTheme: document.querySelector('#theme-switch-toggle'),
 };
 
 // Создание и рендер разметки
@@ -13,4 +14,35 @@ refs.menuContainer.insertAdjacentHTML('beforeend', menuCardsMarkup);
 
 function createMenuCardsMarkup(menu) {
   return menuCardsTpl(menu);
+}
+
+// Функционал изменения темы
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+refs.toggleTheme.addEventListener('change', onToggleThemeChange);
+
+selectedTheme();
+
+function onToggleThemeChange() {
+  if (refs.toggleTheme.checked) {
+    document.body.classList.add(Theme.DARK);
+    document.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    document.body.classList.add(Theme.LIGHT);
+    document.body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function selectedTheme() {
+  const storageTheme = localStorage.getItem('theme');
+  if (storageTheme === Theme.DARK) {
+    document.body.classList.add(Theme.DARK);
+    refs.toggleTheme.checked = true;
+  }
 }
