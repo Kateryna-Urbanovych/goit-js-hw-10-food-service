7797fd60e2a67ed5331a56f6a83724aa4e37b654
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-10-11 15:29:32 +0300

    Добавляет исходные файлы дз и Создает разметку по шаблону

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('qweqweqwe');
+import menuCardsTpl from './templates/menu-cards.hbs';
+import menu from './js/menu.json';
+import './css/styles.css';
+
+const refs = {
+  menuContainer: document.querySelector('.js-menu'),
+};
+
+// Создание и рендер разметки
+const menuCardsMarkup = createMenuCardsMarkup(menu);
+
+refs.menuContainer.insertAdjacentHTML('beforeend', menuCardsMarkup);
+
+function createMenuCardsMarkup(menu) {
+  return menuCardsTpl(menu);
+}
