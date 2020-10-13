8a0e073ee0bc0edce2cc31dee1cd2a9aaca70c8b
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-10-13 14:57:56 +0300

    Разбивает модули

---
+++ src/js/menu-markup.js
@@ -0,0 +1,15 @@
+import menuCardsTpl from '../templates/menu-cards.hbs';
+import menu from './menu.json';
+
+const refs = {
+  menuContainer: document.querySelector('.js-menu'),
+};
+
+// Создание и рендер разметки
+const menuCardsMarkup = createMenuCardsMarkup(menu);
+
+refs.menuContainer.insertAdjacentHTML('beforeend', menuCardsMarkup);
+
+function createMenuCardsMarkup(menu) {
+  return menuCardsTpl(menu);
+}
